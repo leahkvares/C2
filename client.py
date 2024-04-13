fa8f986f1bc7e71c38c8351078bc60afc9d14e9e
Leah Kvares <lk3053@rit.edu>
2024-04-13 16:34:39 -0400

    rearrange files

--- client.py
+++ client.py
@@ -4,17 +4,11 @@ import time
 
 SERVER_URL = "http://127.0.0.1:5000"
 
-# commands = {} # keep history of commands executed on this client?
-# jk its in server now
-
 # client reaches out to server to get told its ID
 def log_client():
-    # get ip
-    ip = subprocess.run("ifconfig | grep 129 | grep inet | awk '{print $2}'", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode().strip()
+    ip = subprocess.run("ifconfig | grep 129 | grep inet | awk '{print $2}'", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode().strip() # get ip
     # hostname -I
     response = requests.get(f"{SERVER_URL}/log-client", params={'client_id': ip}) # send ip
-
-    # response = requests.get(f"{SERVER_URL}/log-client")
     if response.status_code == 200:
         return response.json()['client_id'] # need server to return to the client what its ID is
     else:
