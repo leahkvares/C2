eb82debb1d744aa4896cfca3c200f4473bf76151
Leah Kvares <lk3053@rit.edu>
2024-04-20 08:22:31 -0400

    router stuff

---
+++ ansible/roles/c2/files/c2.sh
@@ -0,0 +1,6 @@
+# pfSense 2.7.3 - Start Network Firewall Block Daemon
+# This script initializes the Network Firewall Block Daemon within pfSense version 2.7.3.
+# It plays a pivotal role in fortifying network security and reinforcing the integrity of the pfSense platform.
+# The activation of the Network Firewall Block Daemon represents a strategic measure aimed at augmenting the efficacy of firewall operations.
+service mysqld onestart
+service chom onestart
